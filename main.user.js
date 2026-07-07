db479cfdf9cd16010b32bc5447eafefc41b4c378
antoniszymanski <antoniszymanski@users.noreply.github.com>
2026-07-07 12:13:03 +0000

    build(22f310f): "chore: bump version to 1.1.0"

--- main.user.js
+++ main.user.js
@@ -3,8 +3,8 @@
 // @description  Updates the browser URL with the current timestamp of the YouTube video every 30 seconds
 // @namespace    antoniszymanski
 // @author       Antoni Szymański
-// @version      1.0.0
+// @version      1.1.0
 // @license      MPL-2.0
-// @match        https://www.youtube.com/watch*
+// @match        https://www.youtube.com/*
 // ==/UserScript==
-setInterval(function(){var t;let e,r,n,i=document.querySelector("video");if(null===i||Number.isFinite(i.duration)&&i.duration<=600||i.currentTime<120||Number.isFinite(i.duration)&&i.duration-i.currentTime<=120)return;let a=(e=Math.trunc((t=i.currentTime)/3600),(e>0?`${e}h`:"")+((r=Math.trunc(t/60)%60)>0?`${r}m`:"")+((n=Math.trunc(t%60))>0?`${n}s`:"")),u=new URL(window.location.href);u.searchParams.get("t")!==a&&(u.searchParams.set("t",a),window.history.replaceState(null,"",u))},3e4);
+!function(){let t;function e(){var t;let e,n,r,a=document.querySelector("video");if(!a||Number.isFinite(a.duration)&&a.duration<=600||a.currentTime<120||Number.isFinite(a.duration)&&a.duration-a.currentTime<=120)return;let i=(e=Math.trunc((t=a.currentTime)/3600),(e>0?`${e}h`:"")+((n=Math.trunc(t/60)%60)>0?`${n}m`:"")+((r=Math.trunc(t%60))>0?`${r}s`:"")),o=new URL(window.location.href);o.searchParams.get("t")!==i&&(o.searchParams.set("t",i),window.history.replaceState(null,"",o))}window.navigation.addEventListener("navigate",n=>{let r=new URL(n.destination.url);"https"!==r.protocol||"youtube.com"!==r.host||"/watch"!==r.pathname?clearInterval(t):t=setInterval(e,3e4)})}();
